e6a0e3e2042995870fee5cee48f8e6f476183058
ssmylh <ssmylh@gmail.com>
2017-06-25 20:14:34 +0900

    Chapter 11, P102 - add sniper status

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -1,6 +1,8 @@
 package auctionsniper.ui;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
@@ -8,10 +10,21 @@ public class MainWindow extends JFrame {
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_LOST = "Lost";
 
+    private JLabel sniperStatus = createLabel(STATUS_JOINING);
+
     public MainWindow() {
         super("Auction Sniper");
         setName(MAIN_WINDOW_NAME);
+        add(sniperStatus);
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private static JLabel createLabel(String initialText) {
+        JLabel label = new JLabel(initialText);
+        label.setName(SNIPER_STATUS_NAME);
+        label.setBorder(new LineBorder(Color.black));
+        return label;
     }
 }
