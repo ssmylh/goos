1141b1927c2a6feda5c775ed0a68eaa04eadd288
ssmylh <ssmylh@gmail.com>
2017-08-06 14:52:26 +0900

    Refactor EndToEndTest

--- src/test/java/auctionsniper/end2end/End2EndTest.java
+++ src/test/java/auctionsniper/end2end/End2EndTest.java
@@ -1,71 +1,72 @@
 package auctionsniper.end2end;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class End2EndTest {
+    FakeAuctionServer auction;
+    ApplicationRunner runner;
+
+    @Before
+    public void constructAuction() throws Exception {
+        this.auction = new FakeAuctionServer("item-54321");
+    }
+
+    @Before
+    public void constructApplicationRunner() {
+        this.runner = new ApplicationRunner();
+    }
+
+    @After
+    public void stopAll() {
+        if (this.auction != null) {
+            this.auction.stop();
+        }
+        if (this.runner != null) {
+            this.runner.stop();
+        }
+     }
 
     @Test
     public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
-        FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-        ApplicationRunner runner = new ApplicationRunner();
-        try {
-            auction.startSellingItem();
-            runner.startBiddingIn(auction);
-            auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
-            auction.announceClosed();
-            runner.hasShownSniperHasLostAuction();
-        } finally {
-            auction.stop();
-            runner.stop();
-        }
+        auction.startSellingItem();
+        runner.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction.announceClosed();
+        runner.hasShownSniperHasLostAuction();
     }
 
     @Test
     public void sniperMakesAHigherBidButLoses() throws Exception {
-        FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-        ApplicationRunner runner = new ApplicationRunner();
+        auction.startSellingItem();
+        runner.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
 
-        try {
-            auction.startSellingItem();
-            runner.startBiddingIn(auction);
-            auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction.reportPrice(1000, 98, "other bidder");
+        runner.hasShownSniperIsBidding();
 
-            auction.reportPrice(1000, 98, "other bidder");
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
 
-            runner.hasShownSniperIsBidding();
-            auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
-
-            auction.announceClosed();
-            runner.hasShownSniperHasLostAuction();
-        } finally {
-            auction.stop();
-            runner.stop();
-        }
+        auction.announceClosed();
+        runner.hasShownSniperHasLostAuction();
     }
 
     @Test
     public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
-        FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-        ApplicationRunner runner = new ApplicationRunner();
+        auction.startSellingItem();
+        runner.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
 
-        try {
-            auction.startSellingItem();
-            runner.startBiddingIn(auction);
-            auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction.reportPrice(1000, 98, "other bidder");
+        runner.hasShownSniperIsBidding();
 
-            auction.reportPrice(1000, 98, "other bidder");
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
 
-            runner.hasShownSniperIsBidding();
-            auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+        auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+        runner.hasShownSniperIsWinning();
 
-            auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
-            runner.hasShownSniperIsWinning();
-
-            auction.announceClosed();
-            runner.hasShownSniperHasWonAuction();
-        } finally {
-            auction.stop();
-            runner.stop();
-        }
+        auction.announceClosed();
+        runner.hasShownSniperHasWonAuction();
     }
 }
